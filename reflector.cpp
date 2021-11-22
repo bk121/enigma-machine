8308fcd945e2315145700a9b5a99976f18258028
Ben Kirwan <bk121@curve03.doc.ic.ac.uk>
2021-11-22 18:17:17 +0000

    sorting 27 parameter reflector

--- reflector.cpp
+++ reflector.cpp
@@ -20,23 +20,23 @@ void Reflector::initialiseReflector(char * reflect_file, int& error_code){
   for (int i=0; i<reflector_file_length && !in.eof(); i++){
     in >> reflector_file_array[i];
 
-   
-    if (in.fail() && !in.eof()){
-      error_code=4;
-      return;
-    }
+    if (i<26){
+      if (in.fail() && !in.eof()){
+	error_code=4;
+	return;
+      }
 
-    if (outsideBounds(reflector_file_array[i])){
+      if (outsideBounds(reflector_file_array[i])){
 	error_code=3;
 	return;
       }
 
-    for (int j=0; j<i; j++){
-      if (reflector_file_array[i]==reflector_file_array[j]){
-	error_code=9;
-	return;
+      for (int j=0; j<i; j++){
+	if (reflector_file_array[i]==reflector_file_array[j]){
+	  error_code=9;
+	  return;
+	}
       }
-
     }
   }
 
